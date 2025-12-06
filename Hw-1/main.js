2690a9ebc76fa3f6bda0981e62c0d568c9dfdd63
u7liss <uliss324@gmail.com>
2025-12-06 19:34:49 +0200

    homework-1

---
+++ Hw-1/main.js
@@ -0,0 +1,72 @@
+// ДЗ:
+
+// #dYQNrBV ------------------------------------------------------------
+// Створити змінні. Присвоїти кожному з них значення: ‘hello’,’owu’,’com’, ‘ua’, 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+// Вивести кожну змінну за допомогою: console.log
+
+let a = 'hello';
+console.log(a);
+let b = 'owu';
+console.log(b);
+let c = 'com';
+console.log(c);
+let d = 'ua';
+console.log(d);
+let f = 1;
+console.log(f);
+let j = 10;
+console.log(j);
+let k = -999;
+console.log(k);
+let l = 123;
+console.log(l);
+let m = 3.14;
+console.log(m);
+let n = 2.7;
+console.log(n);
+let p = 16;
+console.log(p);
+let r = true;
+console.log(r);
+let s = false;
+console.log(s);
+
+
+// #6Qb97gsv -------------------------------------------------------------------
+// – Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З’єднати їх в одну змінну person (Не об’єкт, просто за допомогою конкатенації)
+
+
+// let firstName = 'Viktor';
+// let middleName = 'Viktorovich';
+// let lastName = 'Ihnatenko';
+//
+// // let person = firstName + ' ' + middleName + ' ' + lastName;
+// let person = `${firstName} ${middleName} ${lastName}`;
+// console.log(person);
+//
+
+
+
+// #4N0y5tufA ------------------------------------------------------------------
+// – За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+//   let a = 100; let b = ‘100’; let c = true;
+
+// let a = 100;
+// console.log(typeof a);
+// let b = '100';
+// console.log(typeof b);
+// let c = true;
+// console.log(typeof c);
+
+
+
+
+// #ruUtWDUI---------------------------------------------------------------------
+// Додаткове для тих, хто цікавився prompt`oм
+// – За допомогою 3-х різних prompt() отримати 3 слова які являються вашими Імʼям, По-батькові та роками. Та вивести в консоль
+
+// const firstName = prompt("Введіть ваше ім'я:");
+// const middleName = prompt("Введіть ваше ім'я по-батькові:")
+// const lastName = prompt("Введіть ваше прізвище:");
+
+// console.log(`${firstName} ${middleName} ${lastName}`);
