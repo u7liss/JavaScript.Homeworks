0892e67b2a0d6e35849691614c97afe088e46216
u7liss <uliss324@gmail.com>
2025-12-27 00:14:31 +0200

    correction

--- Hw-4/Hw-4.13/main.js
+++ Hw-4/Hw-4.13/main.js
@@ -19,26 +19,5 @@ function exchange(sumUAH, currencyValues, exchangeCurrency) {
 console.log(exchange(10000, [{ currency: 'USD', value: 25 }, { currency: 'EUR', value: 42 }], 'USD'));
 
 
-// function exchange(sumUAH, currencyValues, exchangeCurrency) {
-//   const currencyObj = currencyValues.find(
-//     item => item.currency === exchangeCurrency
-//   );
 
-//   if (!currencyObj) {
-//     return null; // або можна кинути помилку
-//   }
 
-//   return sumUAH / currencyObj.value;
-// }
-
-// // Приклад використання
-// const result = exchange(
-//   10000,
-//   [
-//     { currency: 'USD', value: 25 },
-//     { currency: 'EUR', value: 42 }
-//   ],
-//   'USD'
-// );
-
-// console.log(result); // 400
